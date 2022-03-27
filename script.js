126cf4687666134fcd6d4590702aa3d62d7f8b54
Shahad Aljehani <shahadaljehani@Shahads-MacBook-Pro.local>
2022-03-27 06:30:08 +0300

    initailcommit

--- script.js
+++ script.js
@@ -11,5 +11,16 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-
+    $('.button:nth-child(1)').on('click', function(){
+        window.location = "https://www.linkedin.com/in/shahad-aljehani/" ;    
+   });
+   $('.button:nth-child(2)').on('click', function(){
+    window.location = "https://twitter.com/shahad_jehani";    
+    });
+    $('.button:nth-child(3)').on('click', function(){
+    window.location = "https://www.behance.net/shahad_jehani";    
+    });
+    $('.button:nth-child(4)').on('click', function(){
+    window.location = "https://github.com/ShahadDev2";    
+    });
 });
